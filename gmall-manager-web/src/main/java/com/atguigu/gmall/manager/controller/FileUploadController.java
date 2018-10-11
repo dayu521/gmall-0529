37d608a71c73bc991a2233ab77116d2aa2f83b18
leifengyang <534096094@qq.com>
2018-10-11 17:20:59 +0800

    fastdfs文件上传测试通过。文件上传功能完成

--- gmall-manager-web/src/main/java/com/atguigu/gmall/manager/controller/FileUploadController.java
+++ gmall-manager-web/src/main/java/com/atguigu/gmall/manager/controller/FileUploadController.java
@@ -1,17 +1,28 @@
 package com.atguigu.gmall.manager.controller;
 
+import com.atguigu.gmall.manager.utils.FastDFSUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.csource.common.MyException;
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.StorageClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 @Slf4j
 @RequestMapping("/file")
 @Controller
 public class FileUploadController {
 
+    @Autowired
+    FastDFSUtils fastDFSUtils;
     /**
      * 文件上传成功返回 文件的实际访问地址
      * @param file
@@ -19,11 +30,23 @@ public class FileUploadController {
      */
     @ResponseBody
     @RequestMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file){
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException, MyException {
 
         if (!file.isEmpty()){
             log.info("文件:{}，大小：{}；上传完成",file.getOriginalFilename(),file.getSize());
+            StorageClient storageClient = fastDFSUtils.getStorageClient();
+            String ext = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+
+            NameValuePair[] pairs = new NameValuePair[1];
+            pairs[0] = new NameValuePair("fileName", file.getOriginalFilename());
+
+            String[] strings = storageClient.upload_file(file.getBytes(), ext, pairs);
+
+            String uploadFileUrl = fastDFSUtils.getUploadFileUrl(strings);
+            return uploadFileUrl;
+
         }
-        return "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+
+        return "images/error.png";
     }
 }
--- gmall-manager-web/src/test/java/com/atguigu/gmall/manager/GmallManagerWebApplicationTests.java
+++ gmall-manager-web/src/test/java/com/atguigu/gmall/manager/GmallManagerWebApplicationTests.java
@@ -1,16 +1,52 @@
 package com.atguigu.gmall.manager;
 
+import org.csource.common.MyException;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallManagerWebApplicationTests {
 
+	/**
+	 * 测试使用FastDFS上传
+	 */
 	@Test
-	public void contextLoads() {
-	}
+	public void contextLoads() throws IOException, MyException {
+		//初始化流程
+		//1、加载配置文件
+		String file = this.getClass().getResource("/tracker.conf").getFile();
+		//2、初始化fastdfs客户端配置
+		ClientGlobal.init(file);
 
+		//上传流程
+		//1、创建TrackerClient
+		TrackerClient trackerClient=new TrackerClient();
+		//2、获取到和TrackerServer的连接
+		TrackerServer trackerServer=trackerClient.getConnection();
+		//3、根据TrackerServer返回的信息创建出操作Storage的客户端
+		StorageClient storageClient=new StorageClient(trackerServer,null);
+
+		//
+		String localFilePath="C:\\Users\\lfy\\Pictures\\upload_3.jpg";
+		//4、使用StorageClient给Storage上传文件
+		String[] upload_file = storageClient.upload_file(localFilePath,"jpg",null);
+
+		for (int i = 0; i < upload_file.length; i++) {
+			String s = upload_file[i];
+			System.out.println("s = " + s);
+		}
+		/**
+		 * s = group1
+		 s = M00/00/00/wKifhFu_edqAONm7ACLptic-Vk0827.bmp
+		 */
+	}
 }
