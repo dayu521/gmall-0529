fccf01e01d20b85d3be5cf3bfc0d992c9efdc3ca
leifengyang <534096094@qq.com>
2018-10-22 21:40:59 +0800

    购物车checkbox

--- gmall-bean/src/main/java/com/atguigu/gmall/cart/CartItem.java
+++ gmall-bean/src/main/java/com/atguigu/gmall/cart/CartItem.java
@@ -1,6 +1,7 @@
 package com.atguigu.gmall.cart;
 
 import com.atguigu.gmall.manager.sku.SkuInfo;
+import com.baomidou.mybatisplus.annotation.EnumValue;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,8 +20,8 @@ public class CartItem implements Serializable {
     @Getter
     private Integer num;//当前项数量
     private BigDecimal totalPrice;//当前项的总价
+
     @Getter
-    @Setter
     private boolean isCheck = false; //可以发ajax请求更新此字段
 
     /**
