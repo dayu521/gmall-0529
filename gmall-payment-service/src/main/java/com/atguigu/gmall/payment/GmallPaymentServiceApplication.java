c422754e73911c51316d3c131f225c3bea1a4cf6
leifengyang <534096094@qq.com>
2018-10-24 12:58:20 +0800

    创建payment项目

---
+++ gmall-payment-service/src/main/java/com/atguigu/gmall/payment/GmallPaymentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallPaymentServiceApplication.class, args);
+	}
+}
